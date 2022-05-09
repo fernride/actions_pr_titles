cddad6814936d1dc36d89157fbc1e9612c24e85e
Maximilian Birkner <maximilian.birkner@fernride.com>
2022-05-09 13:42:50 +0200

    update title to branch name if no jira id was found

--- action.sh
+++ action.sh
@@ -6,6 +6,19 @@
 # "Fs 2345,fs 234" becomes "FS-2345,FS-234"
 # etc.
 title=$(curl -H "Accept: application/vnd.github.v3+json" --header "Authorization: Bearer ${REPO_TOKEN}" https://api.github.com/repos/${REPO_NAME}/pulls/${PR_NUMBER} | grep -Po '"title":.*?[^\\]",')
+title_orig="${title}"
+
+# check if title contains Jira ID already
+if ! grep -o -E "[a-zA-Z0-9,\.\_\-]+-[0-9]+" <<< "${title}"; then
+    echo "Current automatic PR title does not contain Jira Ticket ID, using branch name '${BRANCH_NAME}' instead!"
+    title="${BRANCH_NAME}"
+fi
+
 title=$(sed "s/.*title\": \"\(.*\)\",/\1/g" <<< "${title}")
 title=$(sed "s/\(fs\|FS\|Fs\|fS\)[ -]*\([0-9]*\)/FS-\2/g" <<< "${title}")
-curl -X PATCH -H "Accept: application/vnd.github.v3+json" --header "Authorization: Bearer ${REPO_TOKEN}" "https://api.github.com/repos/${REPO_NAME}/pulls/${PR_NUMBER}" -d "{\"title\":\"$title\"}"
+
+if curl -i --fail -X PATCH -H "Accept: application/vnd.github.v3+json" --header "Authorization: Bearer ${REPO_TOKEN}" "https://api.github.com/repos/${REPO_NAME}/pulls/${PR_NUMBER}" -d "{\"title\":\"$title\"}"; then
+    echo "Updated title from '${title_orig}' to '${title}'."
+else
+    echo "Failed to update PR title from '${title_orig}' to '${title}'."
+fi
