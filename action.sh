cf6ec79d47fd21a3b92ce2f39eb400c471c67351
Maximilian Birkner <maximilian.birkner@fernride.com>
2022-07-12 15:40:49 +0200

    add output to composite action

--- action.sh
+++ action.sh
@@ -9,13 +9,18 @@ title=$(curl -H "Accept: application/vnd.github.v3+json" --header "Authorization
 title_orig="${title}"
 
 # check if title contains Jira ID already, otherwise use branch name (Github often uses a commit message for the PR title)
-if ! grep -o -E "[a-zA-Z0-9,\.\_\-]+-[0-9]+" <<< "${title}"; then
+if ! grep -o -E "[a-zA-Z0-9,\.\_\-]+-[0-9]+" <<<"${title}"; then
     echo "Current automatic PR title does not contain Jira Ticket ID, using branch name '${BRANCH_NAME}' instead!"
     title="${BRANCH_NAME}"
 fi
 
-title=$(sed "s/.*title\": \"\(.*\)\",/\1/g" <<< "${title}")
-title=$(sed "s/\(fs\|FS\|Fs\|fS\)[ -]*\([0-9]*\)/FS-\2/g" <<< "${title}")
+title=$(sed "s/.*title\": \"\(.*\)\",/\1/g" <<<"${title}")
+title=$(sed "s/\(fs\|FS\|Fs\|fS\)[ -]*\([0-9]*\)/FS-\2/g" <<<"${title}")
+
+# get JIRA id from title and set as output for next step
+jira_id=$(grep -o -E "[A-Z0-9]+-[0-9]+")
+echo ::set-output name=jira_id::"${jira_id}"
+echo "Jira id is '${jira_id}'."
 
 if curl -i --fail -X PATCH -H "Accept: application/vnd.github.v3+json" --header "Authorization: Bearer ${REPO_TOKEN}" "https://api.github.com/repos/${REPO_NAME}/pulls/${PR_NUMBER}" -d "{\"title\":\"$title\"}"; then
     echo "Updated title from '${title_orig}' to '${title}'."
